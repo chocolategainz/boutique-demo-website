b18abcc82eb15de7ce74ce987c224cce5f3b798b
Aaron Thomas <05moreminutes@gmail.com>
2025-11-23 15:37:59 +0000

    slight improvement on anchor tag - spacing and font

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="Home">
 {/*Navbar*/}
-    <nav className="navbar flex justify-center justify-evenly bg-cyan-500">
+    <nav className="navbar flex font-medium text-lg  p-6 justify-evenly">
       {/*Logo*/}
     <a href ="#Tops">Tops</a>
     <a href ="#Dresses">Dresses</a>
--- src/app/video-player.js
+++ src/app/video-player.js
@@ -2,9 +2,9 @@
 export default function Video() {
 return (
     <div className="flex justify-center w-auto overflow-visible"> 
-<video autoPlay muted loop className = " w-screen h-[84vh] object-cover"> {/*h-220 object-none w-screen*/}
+<video autoPlay muted loop className = " w-screen h-[84vh] object-cover"> 
     <source src="a-woman-wearing-a-black-dress.mp4" type="video/mp4" />
-    <source src= "a-woma-wearing-a-black-dress.ogg" type = "video/ogg" />
+    <source src= "a-woman-wearing-a-black-dress.ogg" type = "video/ogg" />
     Your browser does not support the video tag
 </video>
 </div>
