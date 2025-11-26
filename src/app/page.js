1a30d7cc54aebdfefd67cd64924c822c26ecbeee
Aaron Thomas <05moreminutes@gmail.com>
2025-11-26 15:12:05 +0000

    centerin logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Video from "./video-player";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="Home">
+{/*Logo*/}
+<div className="logo flex align-center justify-center"> {/*Flex only works on parent containers */}
+   <Image src = "/dream-tank-studios-logo.jpg" width={150} height={150} alt="picture of logo"/>
+</div>
 {/*Navbar*/}
     <nav className="navbar flex font-medium text-lg  p-6 justify-evenly">
-      {/*Logo*/}
     <a href ="#Tops">Tops</a>
     <a href ="#Dresses">Dresses</a>
     <a href ="#Denim">Denim</a>
